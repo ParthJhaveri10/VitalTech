2d910db771eefd30c55a96a0abf0182a608bdbcc
Parth Jhaveri <jhaveriparth10@gmaiil.com>
2024-09-05 23:46:18 +0530

    semifinal

--- components/forms/AppointmentForm.tsx
+++ components/forms/AppointmentForm.tsx
@@ -135,7 +135,7 @@ export const AppointmentForm = ({
           <section className="mb-12 space-y-4">
             <h1 className="header">New Appointment</h1>
             <p className="text-dark-700">
-              Request a new appointment in 10 seconds.
+              Request a new appointment in minutes.
             </p>
           </section>
         )}
